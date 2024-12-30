242ae3657d74dd78bc7320644aef79de2cfbd459
Mohamad Omari <mohamadomari.2546@gmail.com>
2024-12-30 15:19:26 +0300

    update file from the repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:task_app/bloc_observal.dart';
 import 'package:task_app/core/dependency_injection/dependency_injection.dart';
 import 'package:task_app/core/utils/app_router.dart';
 import 'package:task_app/core/utils/themes/dark_theme.dart';
 import 'package:task_app/core/utils/themes/light_theme.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   getItSetup();
